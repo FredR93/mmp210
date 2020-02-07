d97cd8b6941cf51bfdeb61742e6102fb84e51df2
FredR93 <fred.rodriguez001@gmail.com>
2020-02-06 17:36:55 -0800

    new sketch

--- self_portrait/sketch.js
+++ self_portrait/sketch.js
@@ -4,9 +4,52 @@ function setup(){
 }
 
 function draw(){
-	background("lightblue")
-	ellipse(320, 180, 200)
+	background("lightblue");
 
-	ellipse(250, 160, 20);
-	ellipse(350, 160, 20);
+	// shapes with xy coordinate + size
+	
+    fill("red");
+     stroke('black');
+     noStroke()
+
+	// face area
+	ellipse(200, 180, 200);//face 
+    
+    //iner eyes
+    fill(100)
+    noStroke() 
+    ellipse(350, 160, 150)
+    ellipseMode(CENTER);
+    
+    //outer eyes
+    fill('blue')
+	ellipse(350, 160, 100);//right eye
+	fill('yellow')
+	ellipse(600, 160, 350);//left eye 
+
+    //mouth
+    fill('yellow')
+    noStroke()
+	rect(150, 210, 100, 150, 15);
+
+
+	//shapes that are only xy points
+	
+	//hair
+	stroke('black')
+	strokeWeight(3)
+
+	line(100, 70, 160, 500);
+	line(200, 100, 1000, 500);
+	line(170, 100, 0, 100);
+	line(180, 70, 2000, 100);
+
+    //nose
+    fill('black')
+	triangle(200, 180, 100, 150, 220, 220);
+
+	//anything
+	fill('blue') 
+	noStroke()
+	square(30, 20, 55);
 }

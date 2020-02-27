e960ed9e269ba77d4472287cb256840674f6b7df
FredR93 <fred.rodriguez001@gmail.com>
2020-02-27 17:41:24 -0500

    new meme

--- meme/sketch.js
+++ meme/sketch.js
@@ -18,10 +18,16 @@ function setup(){
 
 function draw(){
 	background('gray');
+    textSize(50);
 
 
 	// draw the image
-    tint(255, 0, 255);
-    image(, 0, 0,)
+    tint(350, 150, 100);
+    image(img, 0, 0, 250, 350)
+    tint(300, 255, 255, 127);
+    image(img, 0, 0, mouseX, mouseY);
+
+
+
 }
 

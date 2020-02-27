5eb32db82fd14d7ba7483d0b05cd4255486d329a
FredR93 <fred.rodriguez001@gmail.com>
2020-02-27 16:50:20 -0500

    new interactive

---
+++ meme/sketch.js
@@ -0,0 +1,27 @@
+/*
+   meme
+   */
+
+//gloabal scope
+var dragonImage;
+
+function preload(){
+	img= loadImage("dragon.png")
+}
+
+
+
+function setup(){
+	createCanvas(640, 360);
+}
+
+
+function draw(){
+	background('gray');
+
+
+	// draw the image
+    tint(255, 0, 255);
+    image(, 0, 0,)
+}
+

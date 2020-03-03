445e01d61c065f7555674db3ec1b0f4fdd613906
FredR93 <fred.rodriguez001@gmail.com>
2020-03-03 17:43:54 -0500

    meme2

--- meme2/sketch.js
+++ meme2/sketch.js
@@ -1,12 +1,12 @@
 /*
-   meme
+   meme version2 
    */
 
 //gloabal scope
-var dragonImage;
+var newMeme;
 
 function preload(){
-	img= loadImage("dragon.png")
+	img= loadImage("newmeme.jpg")
 }
 
 
@@ -17,15 +17,15 @@ function setup(){
 
 
 function draw(){
-	background('gray');
-    textSize(50);
+ 
+  
 
+    
 
 	// draw the image
-    tint(350, 150, 100);
-    image(img, 0, 0, 250, 350)
-    tint(300, 255, 255, 127);
-    image(img, 0, 0, mouseX, mouseY);
+    tint(255, 255, 255);
+    image(img, 0, 0, 400, 350)
+    
 
 
 

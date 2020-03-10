9305878bf04a90086919d0ad4b50c66210f2105a
FredR93 <fred.rodriguez001@gmail.com>
2020-03-10 17:27:18 -0400

    update 7

--- meme2/sketch.js
+++ meme2/sketch.js
@@ -3,26 +3,16 @@
    */
 
 //gloabal scope
-var newMeme;
 
-function preload(){
-	img= loadImage("newmeme.jpg")
-}
-
-
-
-function setup(){
-	createCanvas(640, 360);
-}
 
 var x= 100
 var speed= 5; 
 function draw(){
-	background(1000, 1000);
-	img= loadImage("newmeme.jpg")
+	background(350);
+	ellipse(x, 100, 100);
     x += speed;
     if (x > width || x < 0)
-    	{ speed *= -1;}
+    	{speed *= -1;}
 
     
 

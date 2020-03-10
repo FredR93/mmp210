df7df3b0c19979804ab64fcc6982752606f69cbf
FredR93 <fred.rodriguez001@gmail.com>
2020-03-10 17:41:31 -0400

    new update works

--- meme2/sketch.js
+++ meme2/sketch.js
@@ -3,17 +3,25 @@
    */
 
 //gloabal scope
-
+function setup(){
+	createCanvas(640, 360);
+}
 
 var x= 100
 var speed= 5; 
+var r= 0;
+var c= 1; // change 
 function draw(){
-	background(350);
+	background(500);
+	fill( r, 50, 100);
+	noStroke();
 	ellipse(x, 100, 100);
     x += speed;
     if (x > width || x < 0)
     	{speed *= -1;}
-
+    r += c;
+    if (r > 255 || r < 0)
+    	{ c *= -1}
     
 
 

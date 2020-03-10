f350cff7766544e357d4a8ceeacae5cccac7c77e
FredR93 <fred.rodriguez001@gmail.com>
2020-03-10 17:06:16 -0400

    meme2 update

--- meme2/sketch.js
+++ meme2/sketch.js
@@ -15,16 +15,18 @@ function setup(){
 	createCanvas(640, 360);
 }
 
-
+var x= 100
+var speed= 5; 
 function draw(){
- 
-  
+	background(1000, 1000);
+	img= loadImage("newmeme.jpg")
+    x += speed;
+    if (x > width || x < 0)
+    	{ speed *=-1;}
 
     
 
-	// draw the image
-    tint(255, 255, 255);
-    image(img, 0, 0, 400, 350)
+
     
 
 

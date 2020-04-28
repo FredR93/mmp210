f248b76f3a494712becbad48780cb4fc6deedef7
FredR93 <60409079+FredR93@users.noreply.github.com>
2020-04-27 23:00:01 -0400

    new

--- logo2/sketch.js
+++ logo2/sketch.js
@@ -1,75 +1,64 @@
 /*
    logo2 
    */
-
-//gloabal scope
-function setup(){
-	createCanvas(640, 360);
-}
+   //gloabal scope
+var y= 100;
+var n= 100;
 var x= 100
 var speed= 5; 
-var r= 0;
+var a= 0;
 var c= 1; // change 
+var r, g, b;
+
+
+function setup(){
+	createCanvas(640, 360);
+  r= random(0,255);
+  g= random(0,255);
+  b= random(0,255);
+
+  noStroke();
+}
+
 function draw(){
-	background("lightblue");
-	fill( r, 50, 100);
-	noStroke();
-	ellipse(x, 100, 100);
+	// background("lightblue");
+  if(frameCount %25 === 0){
+    r= random(0,255);
+    g= random(0,255);
+    b= random(0,255);
+      }
+
+	
+  fill(r,g,b);
+	ellipse(x, y, n, 100,);
     x += speed;
     if (x > width || x < 0)
     	{speed *= -1;}
-    r += c;
-    if (r > 255 || r < 0)
-    	{ c *= -1}
-    
+    a += c;
+    if (a > 255 || a < 0)
+    	{ c *= 1}
+    y +=2;
+    n +=2;
+    if (y > height)
+      { y= 0;}
+    if (n > height)
+    { n= 0;}
 
 
-  // shapes with xy coordinate + size
-  
-    fill("red");
-     stroke('black');
-     noStroke()
+if (mouseX > width/2) {
+    fill("lightblue");
+  } else {
+    fill("plum");
+  }
 
-  // face area
-  ellipse(200, 180, 200);//face 
-    
-    //iner eyes
-    fill(100)
-    noStroke() 
-    ellipse(350, 160, 150)
-    ellipseMode(CENTER);
-    
-    //outer eyes
-    fill('blue')
-  ellipse(350, 160, 100);//right eye
-  fill('yellow')
-  ellipse(600, 160, 350);//left eye 
-
-    //mouth
-    fill('yellow')
-    noStroke()
-  rect(150, 210, 100, 150, 15);
+  if (mouseIsPressed) {
+    // bigger difference in mouse position means bigger size
+    var s = mouseX - pmouseX; 
+    ellipse(mouseX, mouseY, s);
+  }
 
 
-  //shapes that are only xy points
-  
-  //hair
-  stroke('black')
-  strokeWeight(3)
-
-  line(100, 70, 160, 500);
-  line(200, 100, 1000, 500);
-  line(170, 100, 0, 100);
-  line(180, 70, 2000, 100);
-
-    //nose
-    fill('black')
-  triangle(200, 180, 100, 150, 220, 220);
-
-  //anything
-  fill('blue') 
-  noStroke()
-  square(30, 20, 55);
+ 
 
 
 

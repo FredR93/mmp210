76584e7828ced86b749950c8d245caf9deb33790
FredR93 <60409079+FredR93@users.noreply.github.com>
2020-04-29 18:29:09 -0400

    loop

---
+++ loop/sketch.js
@@ -0,0 +1,17 @@
+/*
+   loop 
+   */
+   // draw 5 circles in a line
+
+function setup(){
+	createCanvas(640, 360);
+    background(220)
+
+//
+ for (var x= 50; x < width; x +=50)
+
+  {circle(x, height/2, 50);}
+
+
+}
+

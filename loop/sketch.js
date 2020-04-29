58579c8a090532f02382ddcbfcacf1b27d269708
FredR93 <60409079+FredR93@users.noreply.github.com>
2020-04-29 19:46:35 -0400

    loop2

--- loop/sketch.js
+++ loop/sketch.js
@@ -10,7 +10,11 @@ function setup(){
 //
  for (var x= 50; x < width; x +=50)
 
-  {circle(x, height/2, 50);}
+  {circle(x, height/2, 50);
+   circle(x - 10, height/2 -10, 10);
+   circle(x + 10, height/2 -10, 10)
+   rect(x, height/2 + 10, 15, 10, 5);
+}
 
 
 }

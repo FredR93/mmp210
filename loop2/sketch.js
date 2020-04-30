29250f2e9ccdb6efb238d5f78b73b82a6fd3a166
FredR93 <60409079+FredR93@users.noreply.github.com>
2020-04-29 20:43:30 -0400

    new loop

---
+++ loop2/sketch.js
@@ -0,0 +1,40 @@
+/*
+   loop2
+   */
+   // draw 5 circles in a line
+
+function setup(){
+	createCanvas(640, 360);
+    noStroke();
+ 
+
+    rotateSlider = createSlider(0, TWO_PI, 0);
+    rotateSlider. input(pattern);
+
+
+	pattern(); // generate a pattern to start
+}
+
+	
+function pattern() {
+	background('plum');
+	
+	fill('lightblue');
+	
+	for (let x = 0; x <= width; x += 50) {
+		ellipse(x, random(height), random(100));
+	
+
+    push();
+    translate(x, height/2);
+    rotate(rotateSlider .value());
+    pop();
+    
+	}
+
+    
+
+
+
+}
+

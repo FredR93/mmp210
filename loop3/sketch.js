6f57eaabe1b6142f322236219931cee41651a4a9
FredR93 <60409079+FredR93@users.noreply.github.com>
2020-05-14 15:22:57 -0400

    new7

--- loop3/sketch.js
+++ loop3/sketch.js
@@ -39,6 +39,10 @@ function setup(){
 		}
 	}
     
+    for (let x = 0; x <= width; x += 50) {
+		rect(x, random(height), random(100));
+
+
     push();
     translate(x, height/2);
     rotate(rotateSlider .value());

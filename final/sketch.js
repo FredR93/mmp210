64893ae03308813bad5462fb169f56c3ff5e713c
FredR93 <60409079+FredR93@users.noreply.github.com>
2020-05-22 23:38:40 -0400

    2

--- final/sketch.js
+++ final/sketch.js
@@ -1,2 +1,25 @@
 
 
+function draw() {
+	createCanvas(640, 360);
+	background("lightblue");
+	button(100, 100, 100);
+	button(200, 200, 200);
+	button(300, 100, 100);
+	button(500, 300, 400);
+	button(320, 180, 50);
+}
+
+function button(x, y, s) {
+	var d = dist(mouseX, mouseY, x, y);
+	if (d < s / 2) {
+		fill('green');
+	} else {
+		fill('blue');
+	}
+	ellipse(x, y, s);
+    
+
+    fill("red");
+    square( x, y, s); 
+}
